0566f1528efad1508db3a8e65320b93ea8320bc2
POLICE <rahulhiran6@gmail.com>
2024-10-12 09:01:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,19 +8,19 @@ if (fs.existsSync('config.env')) {
 }
 
 const API = "https://api-aswin-sparky.koyeb.app";
-const ALIVE = process.env.ALIVE || "I am Alive";
-const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const ALIVE = process.env.ALIVE || "I am Rahul_JI";
+const HANDLERS = process.env.HANDLER || ".";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:ec99389d39d4b5ac8a528cc8cc47ad8b";
+const SUDO = process.env.SUDO || '919356730236';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
 const PM_BLOCK = process.env.PM_BLOCK || "false";
 const PMB = process.env.PMB || "Sorry, I can't help you in private chat.";
 const READ_MESSAGES = process.env.READ_MESSAGES || "false";
-const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://i.imgur.com/r3GZeiX.jpeg";
+const BOT_INFO = process.env.BOT_INFO || "Rahul-police-officers;Rahul_JI;https://i.imgur.com/7Ebd232.jpeg";
 const URL = process.env.URL || "https://www.instagram.com/sparky.drip";
-const AUDIO_DATA = process.env.AUDIO_DATA || "X BOT MD;ASWIN SPARKY;https://i.imgur.com/fVCRCYG.jpeg";
+const AUDIO_DATA = process.env.AUDIO_DATA || "Rahul-police-officers;Rahul_JI;https://i.imgur.com/7Ebd232.jpeg";
 const STICKER_DATA = process.env.STICKER_DATA || "X BOT MD;ASWIN SPARKY";
 const WORK_TYPE = process.env.WORK_TYPE || 'public';
 const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
